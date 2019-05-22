7432e72cfab22f366d6082a7676896518dd439e3
Santhosh Vaddi <santhoshvaddi@google.com>
2019-05-22 07:19:33 -0700

    Initial setup for token authentication framework.

---
+++ FirebaseAuthentication/FirebaseAuthentication/FirebaseAuthentication/FirebaseAuthentication.h
@@ -0,0 +1,19 @@
+//
+//  FirebaseAuthentication.h
+//  FirebaseAuthentication
+//
+//  Created by Santhosh Vaddi on 5/22/19.
+//  Copyright © 2019 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirebaseAuthentication.
+FOUNDATION_EXPORT double FirebaseAuthenticationVersionNumber;
+
+//! Project version string for FirebaseAuthentication.
+FOUNDATION_EXPORT const unsigned char FirebaseAuthenticationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseAuthentication/PublicHeader.h>
+
+
